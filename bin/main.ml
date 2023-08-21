c2aea5fb7fd4bef70b8b39e7e1510e0e12ef5b7e
Francesco Stiffoni <fstiffo@sgajo.com>
2023-08-21 15:42:26 +0200

    Soluution of first part of day 1

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+open Lib
+
+(* let () = print_endline (Int.to_string (Day01.solution_1 "day01.txt")) *)
+
+
+let () =
+  let result = Math.add 2 3 in
+  print_endline (string_of_int result);
+  let result = Math.sub 3 1 in
+  print_endline (string_of_int result)
